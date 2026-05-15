ac6c33f55de44269c16407f5fb08b7d9f9da84b6
Stefan Danzl <stefan.danzl@live.de>
2026-05-15 11:08:53 +0200

    pico added

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <string.h>
+// #include <time.h>
+#include <TinySoftwareSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+soft_ring_buffer buffer;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// TinySoftwareSerial mySerial(&buffer, 2, 1); // RX, TX
+SoftwareSerial mySerial(2, 1); // RX, TX
+
+unsigned long last_time = 0;
+unsigned long INTERVAL = 1000; // 1 Sekunde
+unsigned long BREAKER = 2;
+unsigned long counter = 0;
+
+void setup()
+{
+  mySerial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  if (mySerial.available())
+  {
+    char c = mySerial.read();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (c == '+')
+    {
+      INTERVAL += 100; // +100ms
+      mySerial.print("Interval: ");
+      mySerial.println(INTERVAL);
+    }
+    else if (c == '-')
+    {
+      if (INTERVAL > 100)
+      {                  // Min 100ms
+        INTERVAL -= 100; // -100ms
+      }
+      mySerial.print("Interval: ");
+      mySerial.println(INTERVAL);
+    }
+  }
+
+  unsigned long currentTime = millis();
+  if (currentTime - last_time >= INTERVAL)
+  {
+    mySerial.println("Hello!");
+    last_time = currentTime;
+  }
 }
